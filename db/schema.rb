3393d439b36ee5d764918fb914f972594f33ae02
somakihiro <aki.air@icloud.com>
2017-05-03 10:48:06 +0900

    Add PostsTag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503014307) do
+ActiveRecord::Schema.define(version: 20170503014633) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts_tags", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
