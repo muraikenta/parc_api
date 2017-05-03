1adbe97ec27be543783673148c28d7f81de56918
somakihiro <aki.air@icloud.com>
2017-05-03 10:43:25 +0900

    Add Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503014132) do
+ActiveRecord::Schema.define(version: 20170503014307) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
