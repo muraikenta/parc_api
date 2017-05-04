ce23426eff55458d0a42be8d6eb7d5f331cf6b65
村井謙太 <muraikenta0507@gmail.com>
2017-05-04 17:59:14 +0900

    Associations for Favorite

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -19,4 +19,7 @@ class Favorite < ApplicationRecord
   validates :user, presence: true
   validates :post, presence: true
   validates :user_id, uniqueness: { scope: [:post_id] }
+
+  belongs_to :user
+  belongs_to :post
 end
