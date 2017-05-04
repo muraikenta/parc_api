35bd8bea2225c24a9777868f8b7a778153a3c244
村井謙太 <muraikenta0507@gmail.com>
2017-05-04 11:41:13 +0900

    Add unique constration for tags table

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,6 +7,10 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+# Indexes
+#
+#  index_tags_on_name  (name) UNIQUE
+#
 
 class Tag < ApplicationRecord
   validates :name, presence: true, uniqueness: true
