34e4e621359522d85e8c84dcd69aef4236cae673
chikuwa111 <take0shun1111@gmail.com>
2017-05-05 15:30:06 +0900

    Fix render

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    render :show
+    render :show, formats: 'json', handlers: 'jbuilder'
   end
 
   def follow
     @user = User.find(params[:id])
     current_user.follow!(@user)
-    render :show
+    render :show, formats: 'json', handlers: 'jbuilder'
   end
 
   def unfollow
     @user = User.find(params[:id])
     current_user.unfollow!(@user)
-    render :show
+    render :show, formats: 'json', handlers: 'jbuilder'
   end
 end
