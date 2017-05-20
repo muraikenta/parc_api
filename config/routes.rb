08fc45070406d4cb7a7a11e3c6d4cc1820d459b5
shohohoh <so10ve.q0fsu0u@gmail.com>
2017-05-20 17:13:00 +0900

    override only sessions#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    sessions: 'overrides/sessions',
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: %i[index create] do
     member do
